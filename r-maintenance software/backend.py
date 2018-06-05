d0ed8576fd42153e074e7674d6431799d79bff2d
Milosh Aritonski <30418161+miloshIra@users.noreply.github.com>
2018-06-05 16:08:27 +0200

    Add files via upload
    automating stuff, making my job easier..

---
+++ r-maintenance software/backend.py
@@ -0,0 +1,41 @@
+from datetime import date
+import psycopg2 as psy
+
+link = psy.connect("dbname=maintenancedb" user="postgres" password="post" host="localhost" port="5432")
+cur = link.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS LOGS (Oddel TEXT, Broj INTEGER, Opis TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS TEMP (Oddel TEXT, Broj INTEGER, Opis TEXT)")
+link.commit()
+link.close()
+
+def save_temp_log(oddel, broj, note, date):
+    date = date.today.strftime(%d, %m, %y)
+    link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
+    cur=link.cursor()
+    cur.execute("INSERT INTO TEMP VALUES(%s,%s,s%)",(oddel, broj, note))
+    link.commit()
+    link.close()
+
+def view_temp_logs():
+    link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
+    cur=link.cursor()
+    cur.execute("FROM TEMP SELECT *")
+    link.commit()
+    link.close()
+
+def save_log(oddel, broj, note, date):
+    date = date.today.strftime(%d, %m, %y)
+    link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
+    cur=link.cursor()
+    cur.execute("INSERT INTO LOGS VALUES(%s,%s,s%)",(oddel, broj, note, date))
+    link.commit()
+    link.close()
+
+def view_saved_logs():
+    link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
+    cur=link.cursor()
+    cur.execute("FROM LOGS SELECT *")
+    link.commit()
+    link.close()
+
+# MAYBE ADD Filtering by date later... not .. dont make it more complex than it needs to be. or maybe you are gonna have to have this .. fck me... ...
