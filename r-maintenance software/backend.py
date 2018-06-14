7527149a703ea7b5dca047325159866d518e7ecc
Milosh Aritonski <30418161+miloshIra@users.noreply.github.com>
2018-06-14 16:04:04 +0200

    Did some progress at work

--- r-maintenance software/backend.py
+++ r-maintenance software/backend.py
@@ -3,16 +3,24 @@ import psycopg2 as psy
 
 link = psy.connect("dbname=maintenancedb", user="postgres", password="post", host="localhost", port="5432")
 cur = link.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS LOGS (Oddel TEXT, Broj INTEGER, Opis TEXT, Data INTEGER)")
-cur.execute("CREATE TABLE IF NOT EXISTS TEMP (Oddel TEXT, Broj INTEGER, Opis TEXT, Data INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS LOGS (ID INTEGER, Oddel TEXT, Broj INTEGER, Opis TEXT, Data DATE, Status TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS WORKING (ID INTEGER, Oddel TEXT, Broj INTEGER, Opis TEXT, Data DATE, Status TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS TEMP (ID INTEGER, Oddel TEXT, Broj INTEGER, Opis TEXT, Data DATE, Status TEXT)")
 link.commit()
 link.close()
 
-def save_temp_log(oddel, broj, note, date=None, operator, stauts=None): #datestart datefinish)
+def save_temp_log(oddel, broj, note, date, id=None): #datestart datefinish) oddel, broj, opis, date=None, operator, status=None, ID=None
     # date = date.today()
     link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
     cur=link.cursor()
-    cur.execute("INSERT INTO TEMP VALUES(%s,%s,%s,%s)",(oddel, broj, note, date))
+    cur.execute("INSERT INTO TEMP VALUES(%s,%s,%s,%s %s)",(oddel, broj, note, date, id))
+    link.commit()
+    link.close()
+
+def started_working(status):
+    link = psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
+    cur=link.cursor()
+    cur.execute("UPDATE WORKING SET STATUS=Active")
     link.commit()
     link.close()
 
@@ -23,20 +31,27 @@ def view_temp_logs():
     link.commit()
     link.close()
 
-def save_log(oddel, broj, note, date=None):
+def save_log(oddel, broj, note, date=None, id=None): #date started = date from temp log, date = date finished
     # date = date.today()
     link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
     cur=link.cursor()
-    cur.execute("INSERT INTO LOGS VALUES(%s,%s,%s,%s)",(oddel, broj, note, date))
+    cur.execute("INSERT INTO LOGS VALUES(%s,%s,%s,%s,%s)",(id, oddel, broj, note, date))
     link.commit()
     link.close()
 
-save_log("Pletilici", 1, "asdasdasd")
+# save_log("Pletilici", 1, "Problem so edna glava")
 
 def view_saved_logs():
     link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
     cur=link.cursor()
-    cur.execute("FROM LOGS SELECT *")
+    cur.execute("SELECT * FROM LOGS")
+    link.commit()
+    link.close()
+
+def filter_logs(startdate=None, enddate=None, id=None, Broj=None): #moze ke ima plus filteri ke vidime. Filtriranje po mashina treba da ima isto taka ..
+    link =psy.connect("dbname='maintenancedb' user='postgres' password='post' host='localhost' port='5432'")
+    cur=link.cursor()
+    cur.execute("SELECT FROM LOGS WHERE Data BETWEEN %s AND %s")
     link.commit()
     link.close()
 
--- r-maintenance software/frontend.py
+++ r-maintenance software/frontend.py
@@ -4,11 +4,40 @@ import datetime
 
 
 window=Tk()
-window.title('Maintenance')
+window.title('Пријави дефект')
 window.geometry("500x400+50+50")
 
+b1=Button(window, text = 'Пријави') # Gi pokazuva predhodnite zavrsheni aktivnosti
+b1.place(x=360, y=330, width=120, height=30)
 
+Ovar1 = StringVar(window)
+Ovar1.set("Избери")
 
+Ovar2 = StringVar(window)
+Ovar2.set("Избери")
+
+Ovar3 = StringVar(window)
+Ovar3.set("Избери")
+
+d1=OptionMenu(window, Ovar1, "Pletilici","Ekstruder","Bobinatura","Ekstrakcija", "Benda","Matasa","Drugo")
+d1.place(x=30, y=50, width=100, height=25)
+l1=Label(window, text="Оддел:").place(x=30, y=25, width=80, height=30)
+
+d2=OptionMenu(window, Ovar2, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)
+d2.place(x=160, y=50, width=100, height=25)
+l2=Label(window, text="Машина:").place(x=160, y=25, width=80, height=30)
+
+# d3=OptionMenu(window, Ovar1, "Dragi B.","Nikola N.","Goce C.","Dalibor T.", "Igor G.","Marijan H.")
+# d3.place(x=650, y=350, width=120, height=25)
+# l3=Label(window, text="Оператор:").place(x=555, y=350, width=80, height=30)
+e1 = IntVar()
+e1 = Entry(window)
+e1.place(x=300, y=50, width=40, height=20)
+l2=Label(window, text="Оператор").place(x=286, y=25, width=80, height=30)
+
+e2 = StringVar()
+e2 = Entry(window)
+e2.place(x=30, y=80, width=310, height=280)
 
 
 
